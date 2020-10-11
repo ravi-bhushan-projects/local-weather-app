65b5d6c39c9ee7619428a95296e36631e8adac7f
Ravi Bhushan <ravibhushan122@gmail.com>
2020-10-11 23:38:27 +0530

    Fixed Test Cases

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,25 +1,50 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { WeatherService } from '../weather/weather.service';
 
 import { CurrentWeatherComponent } from './current-weather.component';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>;
 
   beforeEach(async () => {
+    const weatherServiceSpy = jasmine.createSpyObj('WeatherService', ['getCurrentWeather']);
     await TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
+      declarations: [ CurrentWeatherComponent ],
+      providers: [{
+        provide: WeatherService, useValue: weatherServiceSpy
+      }]
     })
     .compileComponents();
+    weatherServiceMock = TestBed.inject(WeatherService) as any;
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
     expect(component).toBeTruthy();
+  });
+
+  it('should get currentWeather from weatherService', () => {
+    // Arrange
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+
+    // Act
+    fixture.detectChanges();
+
+    // Assert
+    expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1);
   });
 });
